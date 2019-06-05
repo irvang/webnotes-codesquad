bd6a7a6726dffa50a060362f1870903d3d410bbb
Irving Angulo <irvang.manage@gmail.com>
2019-06-05 16:57:50 -0400

    Merging for class.

--- js/index.js
+++ js/index.js
@@ -1,43 +1,5 @@
 
-let buttons = document.getElementsByClassName('btn-danger');
-
-/* for (let i = 0; i < buttons.length; i++) {
-  buttons[i].addEventListener('click', function (evt) {
-
-    //same as let index = evt.target.dataset.index (search on web 
-    // for Destructuring Assignment in JavaScript)
-    //we extract the index using the dataset property we stored earlier on 
-    // our button object
-    let { index } = evt.target.dataset;
-
-    // sends request to match route "/notes/:id" , where :id is a parameter
-    // returns a promise that will trigger the .then method when done
-    // the promise returned by fetch is stored in promise1
-    let promise1 = fetch('/notes/' + index, {
-      method: 'DELETE'
-    });
-
-    //triggered when first promise finishes, now stored in promise2
-    let promise2 = promise1.then((response) => {
-
-      //response.json() extracts the JSON data from body. Returning it creates
-      // another promise that will call the next ".then" method when the data is parsed
-      return response.json();
-    });
-
-    promise2.then((jsonResponse) => {//jsonResponse is the parsed data as a JS object
-      location.reload(); //reload page
-
-      //not really doing much since page is reloaded before this point
-      console.log(jsonResponse);
-    })
-      .catch((err) => console.error(err));//if there are any errors, they would be caught on .catch
-
-  });
-} */
-
 let noteForm = document.querySelector('form[id=new-note');
-
 
 noteForm.addEventListener('submit', async (evt) => {
   evt.preventDefault();
@@ -49,7 +11,7 @@ noteForm.addEventListener('submit', async (evt) => {
       method: 'POST',
       body: JSON.stringify({ note: input.value }),
       headers: {
-        "content-type": "application/json"
+        "content-type": "application/json" 
       }
     });
 
@@ -71,6 +33,9 @@ let notesSection = document.querySelector('section#note-cards');
 
 notesSection.addEventListener('click', (evt) => {
   if (evt.target && evt.target.matches('.card-header>button')) {
+
+
+
     //same as let index = evt.target.dataset.index (search on web 
     // for Destructuring Assignment in JavaScript)
     //we extract the index using the dataset property we stored earlier on 
@@ -94,7 +59,8 @@ notesSection.addEventListener('click', (evt) => {
 
     promise2.then((textHtml) => {//textHtml is the parsed data as a JS object
 
-      document.querySelector('section#note-cards').innerHTML = textHtml
+      document.querySelector('section#note-cards').innerHTML = textHtml;
+      // console.log()
     })
       .catch((err) => console.error(err));//if there are any errors, they would be caught on .catch
   }
